0f1745a69091453c8dac78057f4d9dbf1e256769
Muhammad Ashfaq <ashfaq441997@gmail.com>
2023-03-16 19:06:36 +0500

    app deployment in progress on firebase

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+// eslint-disable-next-line node/no-unpublished-require
+const app = require('../app');
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.app = functions.https.onRequest(app);
